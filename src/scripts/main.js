1586d159c102c5ce87d0040211dbec16b7400a62
yahohulia <yaroslavhohulia@gmail.com>
2025-10-22 14:29:53 +0300

    added js-dom solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+let sum = 0;
+let length = 0;
+
+const populations = document.querySelectorAll('span');
+
+for (let i = 0; i < populations.length; i++) {
+  const chunck = populations[i].innerText.split(',');
+  const number = chunck.join('');
+
+  if (typeof +number === 'number' && !Number.isNaN(+number)) {
+    sum += +number;
+    length++;
+  }
+}
+
+const avarage = Math.round(sum / length);
+
+document.querySelectorAll('span.total-population')[0].innerText =
+  sum.toLocaleString('en-US');
+
+document.querySelectorAll('span.average-population')[0].innerText =
+  avarage.toLocaleString('en-US');
